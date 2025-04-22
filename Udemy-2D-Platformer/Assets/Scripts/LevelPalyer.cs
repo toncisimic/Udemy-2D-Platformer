9678da0b23c941f35d6a4ff7a2a0d444c4bf308e
unknown <toncisimic12@gmail.com>
2025-04-22 07:24:17 +0200

    Added Level Select

---
+++ Udemy-2D-Platformer/Assets/Scripts/LevelPalyer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPalyer : MonoBehaviour
+{
+    public MapPoint currentPoint;
+    public float moveSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, currentPoint.transform.position, moveSpeed * Time.deltaTime);
+
+        if (Input.GetAxisRaw("Horizontal") > .5f)
+        {
+           
+            if (currentPoint.right != null)
+            {
+                Debug.Log("DOSAO");
+                SetNextPoint(currentPoint.right);
+            }
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < -.5f)
+        {
+            if (currentPoint.left != null)
+            {
+                SetNextPoint(currentPoint.left);
+            }
+        }
+
+        if (Input.GetAxisRaw("Vertical") > .5f)
+        {
+            if (currentPoint.up != null)
+            {
+                SetNextPoint(currentPoint.up);
+            }
+        }
+
+        if (Input.GetAxisRaw("Vertical") < -.5f)
+        {
+            if (currentPoint.down != null)
+            {
+                SetNextPoint(currentPoint.down);
+            }
+        }
+    }
+
+    public void SetNextPoint(MapPoint nextPoint)
+    {
+        currentPoint = nextPoint;
+    }
+}
---
+++ Udemy-2D-Platformer/Assets/Scripts/MapPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPoint : MonoBehaviour
+{
+    public MapPoint up, down, right, left;
+    public bool isLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
